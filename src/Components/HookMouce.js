b332e0c4aab322456994a8ade6e4b0c2e9de6c1b
saahilch <147494979+saahilch@users.noreply.github.com>
2024-05-30 03:30:56 +0530

    11 commit

--- src/Components/HookMouce.js
+++ src/Components/HookMouce.js
@@ -1,22 +1,23 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 function HookMouce() {
-    const [x,setX]=useState(0)
-    const [y,setY]=useState(0)
-    const logMousePosition =e=>{
+    const [x, setX] = useState(0)
+    const [y, setY] = useState(0)
+    
+    const logMousePosition = e => {
         console.log('Mouse event')
         setX(e.clientX)
         setY(e.clientY)
     }
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect called')
-        window.addEventListener('moucemove', logMousePosition)
-    })
-  return (
-    <div>
-      Hooks X-{x}Y-{y}
-    </div>
-  )
+        window.addEventListener('mousemove', logMousePosition)
+    },[])
+    return (
+        <div>
+            Hooks X-{x} Y-{y}
+        </div>
+    )
 }
 
 export default HookMouce
